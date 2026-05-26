24cb695cc19492f11ea6ad01450ff8e15cf0a782
Shriram Narkhede <shreeramnarkhede.gmail.com>
2026-05-26 17:08:41 +0530

    switch vercel api entry to nodejs runtime

--- api/index.js
+++ api/index.js
@@ -1,9 +1,7 @@
 import server from "../dist/server/server.js";
 
-export const config = {
-  runtime: "edge",
+export default {
+  fetch(request, context) {
+    return server.fetch(request, {}, context);
+  }
 };
-
-export default function handler(request, context) {
-  return server.fetch(request, {}, context);
-}
